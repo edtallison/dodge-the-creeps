24eea0b3c32d9e83cfd0dd00b8918b7f47b8df17
edtallison <edtallison@gmail.com>
2022-11-03 22:33:23 +1300

    Player script: collision handling

--- Player.gd
+++ Player.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+signal hit # custom signal for colliding with enemy
 
 # Member variables
 export var speed = 400 # (pixels/sec). 'export' lets us change it in inspector
@@ -9,6 +10,7 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,9 +33,31 @@ func _process(delta):
 	else:
 		# '$' is shorthand for 'get_node()', at relative path to current node
 		$AnimatedSprite.stop()
-		
+	
+	# change position based on velocity	
 	position += velocity * delta
 	# clamp is used to ensure player stays on screen
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 		
+	# choose animation to play
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+func _on_Player_body_entered(body):
+	hide() # player disappears after being hit
+	emit_signal("hit")
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true) # disable collision after hit
+	
+# for resetting player when starting new game
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
