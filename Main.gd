3ba9d28b0143d15b04f37cc2631298ea9208f2f4
edtallison <edtallison@gmail.com>
2022-11-07 15:25:06 +1300

    Add music and game over sound

--- Main.gd
+++ Main.gd
@@ -21,6 +21,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -30,6 +32,8 @@ func new_game():
 	$HUD.show_message("Get Ready")
 	# remove mobs from last game
 	get_tree().call_group("mobs", "queue_free")
+	
+	$Music.play()
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
