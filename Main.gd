ea3d7fdeeacd1fc6c137c7de9694a47ad25cd1e5
edtallison <edtallison@gmail.com>
2022-11-07 14:29:45 +1300

    Main script timer code

--- Main.gd
+++ Main.gd
@@ -26,3 +26,36 @@ func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+func _on_ScoreTimer_timeout():
+	score += 1
+
+func _on_MobTimer_timeout():
+	# create a new instance of the Mob scene
+	var mob = mob_scene.instance()
+	
+	# choose a random location on Path2D
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.offset = randi()
+	
+	# set the mob's direction perpendicular to the path direction
+	var direction = mob_spawn_location.rotation + PI/2
+	
+	# set the mob's position to the random location
+	mob.position = mob_spawn_location.position
+	
+	# add some randomness to the direction
+	direction += rand_range(-PI/4, PI/4)
+	mob.rotation = direction
+	
+	# choose the velocity for the mob
+	var velocity: Vector2 = Vector2(rand_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	# spawn the mob by adding it to the main scene
+	add_child(mob)
+	
